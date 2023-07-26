a9a0f10749a41e75d9ff03a29b98b1f5c017ab4f
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-07-26 20:18:12 +0700

    Refactor useSetState

--- src/useSetState.ts
+++ src/useSetState.ts
@@ -1,9 +1,9 @@
-import type { AsyncSetter, AsyncState, Setter, State } from 'awai';
+import type { WritableAsyncState, WritableState } from 'awai';
 
-function useSetState<T>(writable: State<T>): Setter<T>;
-function useSetState<T>(writable: AsyncState<T>): AsyncSetter<T>;
-function useSetState<T>(writable: State<T> | AsyncState<T>) {
+const useSetState = <Q extends WritableState<T> | WritableAsyncState<T>, T = any>(
+  writable: Q,
+): Q['set'] => {
   return writable.set;
-}
+};
 
 export default useSetState;
