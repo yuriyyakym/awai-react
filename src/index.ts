abb81ae7e00b72e00256c970a497e9ddca7467d7
Yuriy Yakym <yakym.yuriy@gmail.com>
2023-08-12 17:26:26 +0700

    Add useAsyncStateValue

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { default as useAsyncStateValue } from './useAsyncStateValue';
 export { default as useSetState } from './useSetState';
 export { default as useState } from './useState';
 export { default as useStateValue } from './useStateValue';
---
+++ src/useAsyncStateValue.ts
@@ -0,0 +1,35 @@
+import { type InferReadableType, type ReadableAsyncState, AsyncValue } from 'awai';
+import { useEffect, useState } from 'react';
+
+const useAsyncStateValue = <Q, T extends ReadableAsyncState<Q>>(
+  readable: T,
+): InferReadableType<T> => {
+  const [state, setState] = useState<AsyncValue<Q>>(readable.getAsync);
+
+  useEffect(() => {
+    let mounted = true;
+
+    setState(readable.getAsync());
+
+    (async () => {
+      while (mounted) {
+        /**
+         * @todo Cleanup on unmount
+         * @url https://github.com/yuriyyakym/awai/issues/1
+         */
+        await readable.events.changed;
+        if (mounted) {
+          setState(readable.getAsync());
+        }
+      }
+    })();
+
+    return () => {
+      mounted = false;
+    };
+  }, [readable]);
+
+  return state as InferReadableType<T>;
+};
+
+export default useAsyncStateValue;
