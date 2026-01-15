a1c2bbe56fbd027d2d564c70afab244636986406
Yuriy Yakym <yakym.yuriy@gmail.com>
2026-01-16 01:57:26 +0700

    Refactor `useStateValue`

--- src/useStateValue.ts
+++ src/useStateValue.ts
@@ -1,46 +1,40 @@
 import {
-  type InferReadableType,
+  type AsyncValue,
   type ReadableAsyncState,
   type ReadableState,
   isReadableAsyncState,
 } from 'awai';
-import { startTransition, useEffect, useState } from 'react';
+import { use, useMemo, useSyncExternalStore } from 'react';
+import createSubscribe from './lib/createSubscribe';
+import createGetSnapshot from './lib/createGetSnapshot';
 
-const useStateValue = <T extends ReadableState<any> | ReadableAsyncState<any>>(
-  readable: T,
-): InferReadableType<T> => {
-  const [state, setState] = useState<T | undefined>(readable.get);
+const useStateValue = <T>(readable: ReadableState<T> | ReadableAsyncState<T>): T => {
+  const isAsync = isReadableAsyncState(readable);
+  const getSnapshot = useMemo(() => createGetSnapshot(readable), [readable]);
+  const subscribe = useMemo(() => createSubscribe(readable), [readable]);
+  const snapshot = useSyncExternalStore(subscribe, getSnapshot);
+  const isLoading = isAsync && (snapshot as AsyncValue<T>).isLoading;
 
-  if (isReadableAsyncState(readable) && readable.get() === undefined) {
-    startTransition(() => {
-      throw new Promise((resolve) => readable.events.changed.then(resolve));
-    });
+  const isSettledPromise = useMemo(
+    () => (isLoading ? readable.getPromise() : undefined),
+    [readable, isLoading],
+  );
+
+  if (!isAsync) {
+    return snapshot as T;
   }
 
-  useEffect(() => {
-    let mounted = true;
+  const asyncSnapshot = snapshot as AsyncValue<T>;
 
-    setState(readable.get());
+  if (asyncSnapshot.error) {
+    throw asyncSnapshot.error;
+  }
 
-    (async () => {
-      while (mounted) {
-        /**
-         * @todo Cleanup on unmount
-         * @url https://github.com/yuriyyakym/awai/issues/1
-         */
-        const newValue = await readable.events.changed;
-        if (mounted) {
-          setState(newValue);
-        }
-      }
-    })();
+  if (isSettledPromise) {
+    use(isSettledPromise);
+  }
 
-    return () => {
-      mounted = false;
-    };
-  }, [readable]);
-
-  return state as InferReadableType<T>;
+  return asyncSnapshot.value!;
 };
 
 export default useStateValue;
